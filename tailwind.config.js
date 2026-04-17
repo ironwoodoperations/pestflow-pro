adee103e531b47447f809b4bc098f7834013cca9
csdevore2 <scott@ironwoodoperationsgroup.com>
2026-04-17 18:18:39 +0000

    s142.5 task2: wire tailwind CSS into next.js app directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{ts,tsx}',
+    './shared/**/*.{ts,tsx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
