5b93fc067fbc68e26fd799cea4a9bf51c2ec8738
csdevore2 <scott@ironwoodoperationsgroup.com>
2026-04-18 15:13:56 +0000

    Revert "S149.1: Enable ISR on tenant routes (revalidate=300) for CDN edge caching"
    This reverts commit b8de214dfdc4f1581a389fa2333c11379171cdd8.

--- app/tenant/[slug]/layout.tsx
+++ app/tenant/[slug]/layout.tsx
@@ -1,5 +1,3 @@
-export const revalidate = 300;
-
 import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { resolveTenantBySlug } from '../../../shared/lib/tenant/resolve';
